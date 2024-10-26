10191dc5ee0a8bc8c44e7c7398910fc0e36f3893
thanhdat125 <vothanhdat01234028423@gmail.com>
2024-10-26 15:17:11 +0700

    Initial commit

---
+++ sinhnhat-main/script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "AnhThu" && b=="26102004")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
